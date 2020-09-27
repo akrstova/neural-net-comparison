beb839c72004cbe23833accb67087595e6ba0697
akrstova <krstova.alisa@gmail.com>
2020-09-27 14:02:33 +0200

    Simple GED computation

--- nn-comparison-backend/app/app.py
+++ nn-comparison-backend/app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+import networkx as nx
 from networkx.readwrite import json_graph
+import gmatch4py as gm
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -12,9 +14,12 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def compare_models():
     graphs = request.get_json()
     first_graph = graphs['firstGraph']
+    second_graph = graphs['secondGraph']
     first_graph_x = json_graph.node_link_graph(first_graph)
-    print(first_graph_x)
-    return 'TODO'
+    second_graph_x = json_graph.node_link_graph(second_graph)
+    ged = gm.GraphEditDistance(1, 1, 1, 1)
+    result = ged.compare([first_graph_x, second_graph_x], None)
+    return result
 
 
 if __name__ == '__main__':
