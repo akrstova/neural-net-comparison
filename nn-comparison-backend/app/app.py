480f03ac1f6296b139b3e9e6c8e9970dca557a81
akrstova <krstova.alisa@gmail.com>
2020-09-29 20:18:49 +0200

    Simple comparison of layers

--- nn-comparison-backend/app/app.py
+++ nn-comparison-backend/app/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request
-from flask_cors import CORS, cross_origin
+import itertools
 import networkx as nx
 from networkx.readwrite import json_graph
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
+from app.model.NodeEmbedding import NodeEmbedding
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -17,8 +20,41 @@ def compare_models():
     first_graph_x = json_graph.node_link_graph(first_graph)
     second_graph_x = json_graph.node_link_graph(second_graph)
     result = nx.graph_edit_distance(first_graph_x, second_graph_x, roots=('139798833639120', '139798838186512'))
+    g1_embedded = assign_positions_to_nodes(create_graph_embedding(first_graph_x))
+    g2_embedded = assign_positions_to_nodes(create_graph_embedding(second_graph_x))
+    for node in g1_embedded:
+        all_pairs = list(itertools.product([node], g2_embedded))
+        min = 1000
+        match = None
+        for pair in all_pairs:
+            sim = compute_similarity_nodes(pair[0], pair[1])
+            if sim < min:
+                min = sim
+                match = pair[1]
+        print("Node {} corresponds with node {}".format(node.name, match.name))
     return result
 
+
+def create_graph_embedding(graph):
+    embeddings = []
+    for id in graph.nodes:
+        node = graph.nodes[id]
+        in_degree = graph.in_degree(id)
+        out_degree = graph.out_degree(id)
+        embeddings.append(NodeEmbedding(node['clsName'], in_degree, out_degree))
+        print(in_degree)
+    return embeddings
+
+
+def compute_similarity_nodes(node1, node2):
+    return int(node1.name != node2.name) + abs(node1.in_degree - node2.in_degree) + abs(
+        node1.out_degree - node2.out_degree) + abs(node1.position - node2.position)
+
+
+def assign_positions_to_nodes(graph):
+    for i in range(len(graph)):
+        graph[i].position = i
+    return graph
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
---
+++ nn-comparison-backend/app/model/NodeEmbedding.py
@@ -0,0 +1,7 @@
+class NodeEmbedding:
+    def __init__(self, name, in_degree, out_degree):
+        self.name = name
+        self.in_degree = in_degree
+        self.out_degree = out_degree
+        self.position = 0
+
