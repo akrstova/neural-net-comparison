38cb4dc7ca67585e8f53f06ebd468acc44ae327c
akrstova <krstova.alisa@gmail.com>
2020-11-29 14:34:22 +0100

    Add tooltips

--- nn-comparison-frontend/src/app/components/ng-cyto/ng-cyto.component.ts
+++ nn-comparison-frontend/src/app/components/ng-cyto/ng-cyto.component.ts
@@ -2,9 +2,15 @@ import {Component, OnChanges, Renderer2, ElementRef, Input, Output, EventEmitter
 
 import cytoscape from 'cytoscape';
 import dagre from 'cytoscape-dagre';
-import {first} from "rxjs/operators";
+import popper from 'cytoscape-popper';
+import qtip from 'cytoscape-qtip';
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+
 
 cytoscape.use(dagre);
+cytoscape.use(popper);
+cytoscape.use(qtip);
 
 @Component({
   selector: 'ng2-cytoscape',
@@ -15,10 +21,9 @@ cytoscape.use(dagre);
     position: absolute;
     left: 0;
     top: 0;
-  }`]
+  }
+  `]
 })
-
-
 export class NgCytoComponent implements OnChanges {
 
   @Input() public firstGraph: any;
@@ -43,56 +48,57 @@ export class NgCytoComponent implements OnChanges {
       max: 1.5
     };
 
-    this.style = this.style || cytoscape.stylesheet()
+    this.style = this.style ||
+      cytoscape.stylesheet()
+        .selector('node')
+        .css({
+          'shape': 'data(shapeType)',
+          'width': 'mapData(weight, 40, 80, 20, 60)',
+          'content': 'data(name)',
+          'text-valign': 'center',
+          'text-outline-width': 1,
+          'text-outline-color': 'data(colorCode)',
+          'background-color': 'data(colorCode)',
+          'color': '#fff',
+          'font-size': 10
+        })
+        .selector(':selected')
+        .css({
+          'border-width': 1,
+          'border-color': 'black'
+        })
+        .selector('edge')
+        .css({
+          'curve-style': 'bezier',
+          'opacity': 0.666,
+          'width': 'mapData(strength, 70, 100, 2, 6)',
+          'target-arrow-shape': 'triangle',
+          'line-color': 'data(colorCode)',
+          'source-arrow-color': 'data(colorCode)',
+          'target-arrow-color': 'data(colorCode)'
+        })
+        .selector('edge.questionable')
+        .css({
+          'line-style': 'dotted',
+          'target-arrow-shape': 'diamond'
+        })
+        .selector('.faded')
+        .css({
+          'opacity': 0.25,
+          'text-opacity': 0
+        })
+        .selector('.strong-match')
+        .css({
+          'opacity': 0.5,
+          'background-color': 'green'
+        })
+        .selector('.weak-match')
+        .css({
+            'opacity': 0.25,
+            'background-color': 'green'
+          }
+        )
 
-      .selector('node')
-      .css({
-        'shape': 'data(shapeType)',
-        'width': 'mapData(weight, 40, 80, 20, 60)',
-        'content': 'data(name)',
-        'text-valign': 'center',
-        'text-outline-width': 1,
-        'text-outline-color': 'data(colorCode)',
-        'background-color': 'data(colorCode)',
-        'color': '#fff',
-        'font-size': 10
-      })
-      .selector(':selected')
-      .css({
-        'border-width': 1,
-        'border-color': 'black'
-      })
-      .selector('edge')
-      .css({
-        'curve-style': 'bezier',
-        'opacity': 0.666,
-        'width': 'mapData(strength, 70, 100, 2, 6)',
-        'target-arrow-shape': 'triangle',
-        'line-color': 'data(colorCode)',
-        'source-arrow-color': 'data(colorCode)',
-        'target-arrow-color': 'data(colorCode)'
-      })
-      .selector('edge.questionable')
-      .css({
-        'line-style': 'dotted',
-        'target-arrow-shape': 'diamond'
-      })
-      .selector('.faded')
-      .css({
-        'opacity': 0.25,
-        'text-opacity': 0
-      })
-      .selector('.strong-match')
-      .css({
-        'opacity': 0.5,
-        'background-color': 'green'
-      })
-      .selector('.weak-match')
-      .css({
-        'opacity': 0.25,
-        'background-color': 'green'
-        }
-      );
   }
 
   public ngOnChanges(): any {
@@ -110,29 +116,85 @@ export class NgCytoComponent implements OnChanges {
       elements: this.mergeGraphs(this.firstGraph, this.secondGraph)
     });
 
-    firstGraph.on('click', 'node',(e) => {
+    firstGraph.on('click', 'node', (e) => {
       firstGraph.elements().removeClass('strong-match').removeClass('weak-match');
+      this.destroyAllPoppers();
       const node = e.target;
       const nodeId = node.data('id');
       const matchedNodes = this.nodeMatches[nodeId];
       Object.entries(matchedNodes).forEach(
         ([_, value]) => {
-          const matchedNodeId = value['id'];
           const score = value['score'];
-          if (score > 0.8)
-            firstGraph.elements().filter((elem) => elem.data('id') == matchedNodeId)[0].addClass('strong-match');
-          else
-            firstGraph.elements().filter((elem) => elem.data('id') == matchedNodeId)[0].addClass('weak-match');
+          const matchedNodeId = value['id'];
+          const nodeToHighlight = firstGraph.elements().filter((elem) => elem.data('id') == matchedNodeId)[0];
+          if (score > 0.8) {
+            nodeToHighlight.addClass('strong-match');
+          } else {
+            nodeToHighlight.addClass('weak-match');
+          }
+          this.createPopper(nodeToHighlight, 'right')
         }
       );
     });
 
-    firstGraph.on('click', function (e) {
-      if(e.target.length != 1)
+    firstGraph.on('click', (e) => {
+      if (e.target.length != 1) {
         firstGraph.elements().removeClass('strong-match').removeClass('weak-match');
+        this.destroyAllPoppers();
+      }
     });
 
 
+    firstGraph.on('mouseover', 'node', (e) => {
+      let node = e.target;
+      this.createPopper(node, 'left');
+    });
+
+    firstGraph.on('mouseout', (e) => {
+        // this.destroyAllPoppers();
+      }
+    )
+
+  }
+
+  createPopper(node, placement) {
+    let popper = node.popper({
+      content: () => {
+        const inputShape = node.data('inputShape');
+        const outputShape = node.data('outputShape');
+        let div = document.createElement('div');
+        div.className = 'node-tooltip';
+        div.style.cssText = 'z-index:9999;border:1px solid black;margin-right: 5px;background-color:white;';
+
+        div.innerHTML = '<table>\n' +
+          '                        <tr>\n' +
+          '                        <td>Input shape:</td>\n' +
+          '                        <td>' + inputShape + '</td>\n' +
+          '                        </tr>\n' +
+          '                        <tr>\n' +
+          '                        <td>Output shape:</td>\n' +
+          '                        <td>' + outputShape + '</td>\n' +
+          '                        </tr>\n' +
+          '                        <tr>\n' +
+          '                        </table>';
+
+        document.body.appendChild(div);
+
+        return div;
+      },
+      popper: {
+        'placement': placement
+      } // my popper options here
+    });
+    console.log('Popper', popper);
+    return popper;
+  }
+
+  destroyAllPoppers() {
+    let elements = document.getElementsByClassName('node-tooltip');
+    while (elements.length > 0) {
+      elements[0].parentNode.removeChild(elements[0]);
+    }
   }
 
   mergeGraphs(firstGraph, secondGraph) {
@@ -144,4 +206,46 @@ export class NgCytoComponent implements OnChanges {
     return combined;
   }
 
+  makeTippy(node, text) {
+    var ref = node.popperRef();
+
+    // unfortunately, a dummy element must be passed
+    // as tippy only accepts a dom element as the target
+    // https://github.com/atomiks/tippyjs/issues/661
+    var dummyDomEle = document.createElement('div');
+
+    // @ts-ignore
+    var tip = tippy(dummyDomEle, {
+      onCreate: function (instance) { // mandatory
+        // patch the tippy's popper reference so positioning works
+        // https://atomiks.github.io/tippyjs/misc/#custom-position
+        // @ts-ignore
+        instance.popperInstance.reference = ref;
+      },
+      lazy: false, // mandatory
+      trigger: 'manual', // mandatory
+
+      // dom element inside the tippy:
+      content: function () { // function can be better for performance
+        var div = document.createElement('div');
+
+        div.innerHTML = text;
+
+        return div;
+      },
+
+      // your own preferences:
+      arrow: true,
+      placement: 'bottom',
+      hideOnClick: false,
+      multiple: true,
+      sticky: true,
+
+      // if interactive:
+      interactive: true,
+      appendTo: document.body // or append dummyDomEle to document.body
+    });
+
+    return tip;
+  };
 }
